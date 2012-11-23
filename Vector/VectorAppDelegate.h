0c3fd773ba99ec885392a5c2b95a03f38d70550d
Brian Luczkiewicz <brian@blucz.com>
2012-11-22 20:19:15 -0500

    Initial Commit

---
+++ Vector/VectorAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VectorAppDelegate.h
+//  Vector
+//
+//  Created by Brian Luczkiewicz on 11/22/12.
+//  Copyright (c) 2012 Brian Luczkiewicz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VectorAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Vector/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Vector
+//
+//  Created by Brian Luczkiewicz on 11/22/12.
+//  Copyright (c) 2012 Brian Luczkiewicz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VectorAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VectorAppDelegate class]));
+    }
+}
